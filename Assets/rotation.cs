72e77146d89ab1592c63357b99c5512f6df9bd05
Rogarza04 <rodrigogz2604@gmail.com>
2024-10-14 08:49:50 +0200

    14/10

---
+++ Assets/rotation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    float velocidad = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(10, 0, 0)* velocidad * Time.deltaTime);
+    }
+}
