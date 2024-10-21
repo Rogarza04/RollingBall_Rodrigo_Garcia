8a2de9be855c1884db07ea7cec647fdf547072f5
Rogarza04 <rodrigogz2604@gmail.com>
2024-10-21 20:17:58 +0200

    nivel

--- Assets/GameRespawn.cs
+++ Assets/GameRespawn.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class GameRespawn : MonoBehaviour
 {
-    [SerializeField] float threshold;
+    [SerializeField] GameObject player;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] float spawnTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if(transform.position.y < threshold)
+        if (player.transform.position.y < -spawnTime)
         {
-            transform.position = new Vector3(0.01118958f, 0.360000014f, -0.980000019f);
+            RespawnPoint();
         }
     }
+
+    void RespawnPoint()
+    {
+        transform.position = spawnPoint.position;
+    }
+
 }
